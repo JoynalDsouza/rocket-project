2559497d5d7a82207c1c5a8272e73dc152ce349c
Joynal <joynal@leaguex.com>
2022-11-26 22:04:57 +0530

    serve static folder code

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const planetsRouter = require("./routes/planets/planets.router");
 
@@ -14,7 +15,12 @@ app.use(
 
 //json parser middleware
 app.use(express.json());
+//serve static files from public folder
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.use("/planets", planetsRouter);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
