d675d043bfe97b1510334affeb9b661c829679c8
Joynal <joynal@leaguex.com>
2022-11-27 20:32:55 +0530

    /lauches post request test cases

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -10,3 +10,56 @@ describe("Test /GET Launches", () => {
       .expect("Content-Type", /json/);
   });
 });
+
+describe("Test /POST Launches", () => {
+  const launchData = {
+    mission: "Test mission",
+    target: "Mars",
+    rocket: "FX120F",
+    launchDate: "May 23,2032",
+  };
+  const launchDataWithoutDate = {
+    mission: "Test mission",
+    target: "Mars",
+    rocket: "FX120F",
+  };
+
+  test("should return 201 created", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(launchData)
+      .expect(201)
+      .expect("Content-Type", /json/);
+
+    const launchDate = new Date(launchData.launchDate).valueOf();
+    const responseLaunchDate = new Date(response.body.launchDate).valueOf();
+
+    expect(response.body).toMatchObject(launchDataWithoutDate);
+
+    expect(launchDate).toBe(responseLaunchDate);
+  });
+
+  test("should catch missing required properties", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(launchDataWithoutDate)
+      .expect(400)
+      .expect("Content-Type", /json/);
+
+    expect(response.body).toStrictEqual({
+      error: "Missing required launch property",
+    });
+  });
+
+  test("should catch invalid date", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send({ ...launchDataWithoutDate, launchDate: "hello" })
+      .expect(400)
+      .expect("Content-Type", /json/);
+
+    expect(response.body).toStrictEqual({
+      error: "Invalid launch date",
+    });
+  });
+});
